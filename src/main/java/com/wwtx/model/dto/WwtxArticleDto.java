c2e6d4951bd5c2ba25fff268aa07f7da9ff27277
14202241@qq.com <14202241@qq.com>
2018-04-26 08:25:25 +0800

    1

--- src/main/java/com/wwtx/model/dto/WwtxArticleDto.java
+++ src/main/java/com/wwtx/model/dto/WwtxArticleDto.java
@@ -9,8 +9,33 @@ import java.util.List;
 
 public class WwtxArticleDto extends WwtxArticle {
 
+    private String timg;
+
     private List<WwtxArticleImg> imgList;
 
     private List<WwtxReply> replyList;
 
+    public String getTimg() {
+        return timg;
+    }
+
+    public void setTimg(String timg) {
+        this.timg = timg;
+    }
+
+    public List<WwtxArticleImg> getImgList() {
+        return imgList;
+    }
+
+    public void setImgList(List<WwtxArticleImg> imgList) {
+        this.imgList = imgList;
+    }
+
+    public List<WwtxReply> getReplyList() {
+        return replyList;
+    }
+
+    public void setReplyList(List<WwtxReply> replyList) {
+        this.replyList = replyList;
+    }
 }
